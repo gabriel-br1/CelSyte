f5d0ecd1ac0e843ee39bb1d963e2d23551667fcf
Gabriel B <bober.gabriel1@gmail.com>
2026-03-27 17:25:35 +0100

    implement aspnetcore identity ui authentication

---
+++ CelSyte/Data/CelSyteContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using CelSyte.Models;
+
+namespace CelSyte.Data
+{
+    public class CelSyteContext(DbContextOptions<CelSyteContext> options) : IdentityDbContext<User>(options)
+    {
+    }
+}
--- CelSyte/Program.cs
+++ CelSyte/Program.cs
@@ -1,10 +1,47 @@
-using CelSyte.Components;
+﻿using CelSyte.Components;
+using CelSyte.Components.Account;
+using CelSyte.Data;
+using CelSyte.Models;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("CelSyteContext") ?? throw new InvalidOperationException("Connection string 'CelSyteContext' not found.");;
+
+builder.Services.AddDbContext<CelSyteContext>(options => options.UseSqlServer(connectionString));
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddCascadingAuthenticationState();
+
+builder.Services.AddScoped<IdentityRedirectManager>();
+
+builder.Services.AddScoped<AuthenticationStateProvider, IdentityRevalidatingAuthenticationStateProvider>();
+
+builder.Services.AddAuthentication(options =>
+    {
+        options.DefaultScheme = IdentityConstants.ApplicationScheme;
+        options.DefaultSignInScheme = IdentityConstants.ExternalScheme;
+    })
+    .AddIdentityCookies();
+
+builder.Services.AddIdentityCore<User>(options =>
+    {
+        options.SignIn.RequireConfirmedAccount = true;
+        options.Stores.SchemaVersion = IdentitySchemaVersions.Version3;
+        options.Password.RequireNonAlphanumeric = false;
+        options.Password.RequireDigit = false;
+        options.Password.RequireLowercase = false;
+        options.Password.RequireUppercase = false;
+    })
+    .AddEntityFrameworkStores<CelSyteContext>()
+    .AddSignInManager()
+    .AddDefaultTokenProviders();
+
+builder.Services.AddSingleton<IEmailSender<User>, IdentityNoOpEmailSender>();
 
 var app = builder.Build();
 
@@ -19,5 +56,7 @@ app.UseAntiforgery();
 app.MapStaticAssets();
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
+
+app.MapAdditionalIdentityEndpoints();;
 
 app.Run();
